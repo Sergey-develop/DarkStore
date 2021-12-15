ce1663f251e58274b83fd260af4c63ef82a128c4
Sergey-develop <84603174+Sergey-develop@users.noreply.github.com>
2021-12-15 17:02:25 +0300

    landing
    landing adaptive page with form

---
+++ scripts/script.js
@@ -0,0 +1,37 @@
+$(document).on('ready', function() {
+    $('.slider').slick({
+        dots: false,
+        nextArrow: $(".fas.fa-chevron-right"),
+        prevArrow: $(".fas.fa-chevron-left"),
+        infinite: this,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
+
